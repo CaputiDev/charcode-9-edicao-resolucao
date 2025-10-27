e1690cd33cbb66f3779f7114a3d66bb191af7f89
Caputi <lcaputi2@gmail.com>
2025-10-27 13:43:27 -0300

    feat: add completed ceramica question

--- ceramica/ceramica.js
+++ ceramica/ceramica.js
@@ -4,10 +4,16 @@ const INPUT = fs.readFileSync('./ceramica/input','utf-8');
 
 const LINHAS = INPUT.split('\n');
 
+const PI = 3.1415;
+
 const [DIAMETRO , ALTURA] = LINHAS[0].split(' ');
 
+const RAIO = DIAMETRO/2;
 
+let VOLUME = ((PI * (RAIO ** 2) * ALTURA) * 2) * 1.10;
 
-console.log(altura);
+VOLUME = Math.ceil(VOLUME /5) *5;
+
+console.log(VOLUME);
 
 
